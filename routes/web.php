48244e91280fb781b976bf0b344991ad897a3fd1
Ydavidovici <ydavidovici@gmail.com>
2025-04-03 14:34:53 -0400

    web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\BrandController;
-use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
 
 /*
