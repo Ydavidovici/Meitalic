cf74d7d7a4e69942f3a08609e3c8279a735abb9b
Ydavidovici <ydavidovici@gmail.com>
2025-07-01 15:45:25 -0400

    made insta section text black

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -646,23 +646,34 @@
         </div>
     </section>
 
-
     <section class="instagram py-16 bg-aqua">
         <div class="container mx-auto px-6 text-center flex flex-col items-center">
             <h2 class="text-3xl font-bold mb-4">Follow Us on Instagram</h2>
+
             <a
                 href="https://www.instagram.com/Meitaliccosmeceuticalsnewyork"
                 target="_blank"
                 rel="noopener noreferrer"
-                class="inline-flex items-center space-x-2 justify-center text-accent hover:underline mt-2"
+                class="inline-flex items-center space-x-2 justify-center
+             text-black      <!-- both icon & text are black -->
+             hover:underline mt-2"
             >
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5a4.25 4.25 0 0 0 4.25-4.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5zM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm0 1.5a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7zm4.75-.75a1.25 1.25 0 1 1 0 2.5 1.25 1.25 0 0 1 0-2.5z"/>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="w-8 h-8 text-black"   <!-- force icon black -->
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                >
+                <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5a4.25 4.25 0 0 0 4.25-4.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5zM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm0 1.5a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7zm4.75-.75a1.25 1.25 0 1 1 0 2.5 1.25 1.25 0 0 1 0-2.5z"/>
                 </svg>
-                <span class="text-xl break-all">@Meitaliccosmeceuticalsnewyork</span>
+
+                <span class="text-xl break-all text-black">
+        @Meitaliccosmeceuticalsnewyork
+      </span>
             </a>
         </div>
     </section>
+
 
 
     <!-- About -->
